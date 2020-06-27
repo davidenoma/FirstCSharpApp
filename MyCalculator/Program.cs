bad9341f6ffdf5ce2868c757b40976d9617b2372
david <davidenoma@gmail.com>
2020-06-27 00:51:31 -0700

    First code

--- MyCalculator/Program.cs
+++ MyCalculator/Program.cs
@@ -8,9 +8,21 @@ namespace MyCalculator
         private const double _MAX_VALUE_Double = 1000;  
         static void Main(string[] args)
         {
+            double firstDouble = 0;
+            double secondDouble = 0;
+            double totalDouble = 0;
+            bool OKBool      = true;
             Console.WriteLine("My Calculator");
-            Console.Write("Press enter to exit the program");
-                    
+            
+            Console.WriteLine("Enter first Number");
+
+            firstDouble =   double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            secondDouble = double.Parse(Console.ReadLine());
+            totalDouble = firstDouble  + secondDouble;
+            Console.WriteLine("\n{0} + {1} = {2}", firstDouble, secondDouble, totalDouble);
+            Console.Write("press enter to exit");
+            Console.ReadLine();
         }
     }
 }
