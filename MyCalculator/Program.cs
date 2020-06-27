8278047da7a0d1caeec617e26bc29b531e835f25
david <davidenoma@gmail.com>
2020-06-27 00:15:37 -0700

    first CSharp Appliaction

---
+++ MyCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyCalculator
+{
+    class Program
+    {
+        private const double _MIN_VALUE_Double = -1000;
+        private const double _MAX_VALUE_Double = 1000;  
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Calculator");
+            Console.Write("Press enter to exit the program");
+                    
+        }
+    }
+}
